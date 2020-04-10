1ba3e1ab5d1ef3a6a7f2c0f597fa7356a2089857
Michael Kirk <michael.code@endoftheworl.de>
2020-04-10 14:29:25 -0600

    echo stdin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{ self, BufRead };
+
+fn main() -> io::Result<()> {
+    let mut i = 0;
+    for line in io::stdin().lock().lines() {
+        i += 1;
+        println!("line {}: {}", i, line.ok().unwrap());
+    }
+    Ok(())
 }
+
