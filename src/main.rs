9d5c8065f3e20df28ff207bc733690b2c6c9db43
Michael Kirk <michael.code@endoftheworl.de>
2020-04-13 16:11:45 -0600

    pass through proper params

--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     match args.len() {
         2 => {
-            run(&args[1]).await
+            run(&args[1],Calculation::DirectFromP1).await
         },
         _ => {
             usage();
@@ -43,7 +43,7 @@ WHERE:
 ");
 }
 
-async fn run(bin_name: &String) -> Result<(), Box<dyn std::error::Error>> {
+async fn run(bin_name: &String, calc: Calculation) -> Result<(), Box<dyn std::error::Error>> {
     let mut test_case_reader = BufReader::new(tokio::io::stdin()).lines();
 
     let mut geodsolve_proc = Command::new(bin_name)
@@ -61,9 +61,8 @@ async fn run(bin_name: &String) -> Result<(), Box<dyn std::error::Error>> {
     ).lines();
 
     while let Some(test_case_line) = test_case_reader.next_line().await? {
-        //eprintln!("read test_case_line: {}", test_case_line);
-        geodsolve_input.write_all(test_case_line.as_bytes()).await.expect("write failed");
-        geodsolve_input.write_all("\n".as_bytes()).await.expect("write2 failed");
+        let input = parse_input(&test_case_line, calc);
+        geodsolve_input.write_all(input.as_bytes()).await.expect("write failed");
         geodsolve_input.flush().await.expect("flush failed");
 
         match geodsolve_output.next_line().await {
@@ -83,5 +82,19 @@ fn compare(test_case: String, geodsolve_output: String) {
     // eprintln!("test_case: {} -> geodsolve_output_line: {}", test_case, geodsolve_output);
     println!("{}", geodsolve_output);
     ()
+}
+
+#[derive(Clone, Copy)]
+enum Calculation {
+    DirectFromP1
+}
+
+fn parse_input(line: &str, calc: Calculation) -> String {
+    match calc {
+        Calculation::DirectFromP1 => {
+            let fields: Vec<&str> = line.split(" ").collect::<Vec<&str>>();
+            format!("{} {} {} {}\n", fields[0], fields[1], fields[2], fields[6])
+        }
+    }
 }
 
