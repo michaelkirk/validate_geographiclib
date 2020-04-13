801941355e634d59d31208c7ebced3a85a85721b
Michael Kirk <michael.code@endoftheworl.de>
2020-04-13 12:25:24 -0600

    var renames

--- src/main.rs
+++ src/main.rs
@@ -5,35 +5,36 @@ use std::process::Stdio;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut cmd = Command::new("bin/times_2");
+    let mut geodsolve_cmd = Command::new("bin/times_2");
 
     // Specify that we want the command's standard output piped back to us.
     // By default, standard input/output/error will be inherited from the
     // current process (for example, this means that standard input will
     // come from the keyboard and standard output/error will go directly to
     // the terminal if this process is invoked from the command line).
-    cmd.stdout(Stdio::piped());
+    geodsolve_cmd.stdout(Stdio::piped());
 
-    let mut child = cmd.spawn()
+    let mut geodsolve_proc = geodsolve_cmd.spawn()
         .expect("failed to spawn command");
 
-    let stdout = child.stdout.take()
+    let geodsolve_stdout = geodsolve_proc.stdout.take()
         .expect("child did not have a handle to stdout");
 
-    let mut reader = BufReader::new(stdout).lines();
+    let mut geodsolve_reader = BufReader::new(geodsolve_stdout).lines();
 
     // Ensure the child process is spawned in the runtime so it can
     // make progress on its own while we await for any output.
     tokio::spawn(async {
-        let status = child.await
-            .expect("child process encountered an error");
+        let status = geodsolve_proc.await
+            .expect("geodsolve_proc process encountered an error");
 
-        println!("child status was: {}", status);
+        println!("geodsolve_proc status was: {}", status);
     });
 
-    while let Some(line) = reader.next_line().await? {
+    while let Some(line) = geodsolve_reader.next_line().await? {
         println!("Line: {}", line);
     }
 
     Ok(())
 }
+
