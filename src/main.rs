017cb2d080e28c57cb984f987102a48a9bbeab6a
Michael Kirk <michael.code@endoftheworl.de>
2020-04-13 11:30:48 -0600

    tokio based

--- src/main.rs
+++ src/main.rs
@@ -1,135 +1,39 @@
-use std::io::{self, BufRead, stdin};
-use std::error::Error;
-use std::io::prelude::*;
-use std::process::{Command, Stdio};
+use tokio::io::{BufReader, AsyncBufReadExt};
+use tokio::process::Command;
 
-static PANGRAM: &'static str =
-    "the quick brown fox jumped over the lazy dog\n";
+use std::process::Stdio;
 
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::new("bin/times_2");
 
-fn main() -> io::Result<()> {
-    // wc()?;
-    geodsolve()
-}
+    // Specify that we want the command's standard output piped back to us.
+    // By default, standard input/output/error will be inherited from the
+    // current process (for example, this means that standard input will
+    // come from the keyboard and standard output/error will go directly to
+    // the terminal if this process is invoked from the command line).
+    cmd.stdout(Stdio::piped());
 
-fn wc() -> io::Result<()> {
-    // Spawn the `wc` command
-    let process = match Command::new("wc")
-        .stdin(Stdio::piped())
-        .stdout(Stdio::piped())
-        .spawn() {
-        Err(why) => panic!("couldn't spawn wc: {}", why),
-        Ok(process) => process,
-    };
+    let mut child = cmd.spawn()
+        .expect("failed to spawn command");
 
-    // Write a string to the `stdin` of `wc`.
-    //
-    // `stdin` has type `Option<ChildStdin>`, but since we know this instance
-    // must have one, we can directly `unwrap` it.
-    match process.stdin.unwrap().write_all(PANGRAM.as_bytes()) {
-        Err(why) => panic!("couldn't write to wc stdin: {}",
-                           why),
-        Ok(_) => println!("sent pangram to wc"),
-    }
+    let stdout = child.stdout.take()
+        .expect("child did not have a handle to stdout");
 
-    // Because `stdin` does not live after the above calls, it is `drop`ed,
-    // and the pipe is closed.
-    //
-    // This is very important, otherwise `wc` wouldn't start processing the
-    // input we just sent.
+    let mut reader = BufReader::new(stdout).lines();
 
-    // The `stdout` field also has type `Option<ChildStdout>` so must be unwrapped.
-    let mut s = String::new();
-    match process.stdout.unwrap().read_to_string(&mut s) {
-        Err(why) => panic!("couldn't read wc stdout: {}",
-                           why),
-        Ok(_) => print!("wc responded with:\n{}", s),
+    // Ensure the child process is spawned in the runtime so it can
+    // make progress on its own while we await for any output.
+    tokio::spawn(async {
+        let status = child.await
+            .expect("child process encountered an error");
+
+        println!("child status was: {}", status);
+    });
+
+    while let Some(line) = reader.next_line().await? {
+        println!("Line: {}", line);
     }
 
     Ok(())
 }
-
-fn geodsolve() -> io::Result<()> {
-    // TODO pass as argument
-    let geodsolve_bin = "bin/times_2";
-
-    let mut geodsolve_process = Command::new(geodsolve_bin)
-        .stdin(Stdio::piped())
-        .stdout(Stdio::piped())
-        .spawn()
-        .expect("failed to spawn geodsolve bin");
-
-    let stdin = geodsolve_process.stdin.as_mut().unwrap();
-    let mut line_number = 0;
-    for line in io::stdin().lock().lines() {
-        line_number += 1;
-        let line = line.unwrap();
-        match stdin.write_all(line.as_bytes()) {
-            Err(why) => panic!("couldn't write to wc stdin: {}",
-                               why),
-            Ok(_) => println!("sent line to wc"),
-        }
-        match stdin.write_all("\n".as_bytes()) {
-            Err(why) => panic!("couldn't write to wc stdin: {}",
-                               why),
-            Ok(_) => println!("sent line to wc"),
-        }
-    }
-
-    println!("will wait");
-    geodsolve_process.wait();
-    println!("did wait");
-    // geodsolve_process.stdin.as_mut().unwrap().write_all("\n".as_bytes());
-
-    //}
-
-
-    //
-    //     // Write a string to the `stdin` of `wc`.
-    //     //
-    //     // `stdin` has type `Option<ChildStdin>`, but since we know this instance
-    //     // must have one, we can directly `unwrap` it.
-    //     match geodsolve_stdin.write_all(&line.as_bytes()) {
-    //         Err(why) => panic!("couldn't write to stdin: {}", why),
-    //         Ok(_) => println!("sent line {} to geodsolve", line_number),
-    //     }
-    // }
-
-    // The `stdout` field also has type `Option<ChildStdout>` so must be unwrapped.
-    let mut s = String::new();
-    match geodsolve_process.stdout.unwrap().read_to_string(&mut s) {
-        Err(why) => panic!("couldn't read wc stdout: {}",
-                           why),
-        Ok(_) => print!("wc responded with:\n{}", s),
-    }
-
-    //let _result = child.wait().unwrap();
-
-
-    // Because `stdin` does not live after the above calls, it is `drop`ed,
-    // and the pipe is closed.
-    //
-    // This is very important, otherwise `wc` wouldn't start processing the
-    // input we just sent.
-
-    // The `stdout` field also has type `Option<ChildStdout>` so must be unwrapped.
-    // let mut s = String::new();
-    // match geodsolve_stdout.read_to_string(&mut s) {
-    //     Err(why) => panic!("couldn't read stdout: {}", why),
-    //     Ok(_) => print!("responded with:\n{}", s),
-    // }
-
-    // The `stdout` field also has type `Option<ChildStdout>` so must be unwrapped.
-    // let mut s = String::new();
-    // match geodsolve_process.stdout.unwrap().read_to_string(&mut s) {
-    //     Err(why) => panic!("couldn't read wc stdout: {}",
-    //                        why),
-    //     Ok(_) => print!("geodsolve responded with:\n{}", s),
-    // }
-    //
-    // println!("output: {}", s);
-    // }
-
-    Ok(())
-}
-
